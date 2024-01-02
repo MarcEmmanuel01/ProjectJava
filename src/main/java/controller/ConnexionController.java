9eff65ca40e9cbf5d405009f7575e360643c63e3
MarcEmmanuel01 <emmanuelkassi07@gmail.com>
2024-01-02 12:34:05 +0000

    Premier commit

---
+++ src/main/java/controller/ConnexionController.java
@@ -0,0 +1,52 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class ConnexionController {
+
+    @FXML
+    private JFXButton btnseconnecter;
+
+    @FXML
+    private ImageView imgfermeconnexion;
+
+    @FXML
+    private ImageView imglogo;
+
+    @FXML
+    private Label txtfieldbienvenue;
+
+    @FXML
+    private Label txtfieldcdvr;
+
+    @FXML
+    private JFXTextField txtfieldiden;
+
+    @FXML
+    private Label txtfieldinscrire;
+
+    @FXML
+    private JFXTextField txtfieldmdp;
+
+    @FXML
+    void btnseconnecter(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imgfermeconnexion(MouseEvent event) {
+
+    }
+
+    @FXML
+    void txtfieldinscrire(MouseEvent event) {
+
+    }
+
+}
+
